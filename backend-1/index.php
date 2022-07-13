326b238f3030e53da8bcd9cb3a9c7f3ceacb6cc4
vj1229 <109221609+vj1229@users.noreply.github.com>
2022-07-13 19:54:24 +0700

    Add files via upload

---
+++ backend-1/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+<h1>Welcome <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?></h1>
+   <br>
+
+    <?php echo $_POST["email"]; ?>
+<br>
+
+    
+   
+  
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
